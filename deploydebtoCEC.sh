821f0613347a00d502bc65eed8204e0907a5253a
YEDENDRA B. SHRINIVASAN <yshrini@us.ibm.com>
2016-11-30 15:54:15 -0500

    fix: clearing previous version of the debian package

--- deploydebtoCEC.sh
+++ deploydebtoCEC.sh
@@ -4,6 +4,13 @@ PACKAGENAME=$(grep -m1 name package.json | awk -F: '{ print $2 }' | sed 's/[", ]
 ARCH=amd64
 user=deploy-user
 remote=cec-pkgs.austin.ibm.com
+echo "Removing old package : ${PACKAGENAME}_${VERSION}_${ARCH}.deb, if any"
+ssh ${user}@${remote} rm  /home/${user}/apps/${PACKAGENAME}_${VERSION}_${ARCH}.deb
+echo "cleaning Freight Cache"
+ssh ${user}@${remote} rm /var/cache/freight/dists/xenial/.refs/main/${PACKAGENAME}_${VERSION}_${ARCH}.deb
+ssh ${user}@${remote} rm /var/cache/freight/pool/xenial/main/d/${PACKAGENAME}/${PACKAGENAME}_${VERSION}_${ARCH}.deb
+echo "copying the new package to repo"
 scp dist/${PACKAGENAME}_${VERSION}_${ARCH}.deb ${user}@${remote}:~/apps
+echo "Building Freight cache"
 ssh ${user}@${remote} freight add  /home/${user}/apps/${PACKAGENAME}_${VERSION}_${ARCH}.deb apt/xenial
 ssh ${user}@${remote} freight cache -p /home/${user}/gpg-passphrase
