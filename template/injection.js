0545aad723ceb233a3f772e75a576b8d837c9735
Matthew Peveler <pevelm@rpi.edu>
2018-08-15 14:55:48 -0400

    Add missing createElement line

--- template/injection.js
+++ template/injection.js
@@ -9,6 +9,7 @@ ipcRenderer.on('start_injection', (event, message) => {
     // injecting any additional JS scripts and setting up
     // the listeners, etc. so that we never have to touch this
     // script again.
+    let script = document.createElement('script');
     script.src = `${window.liaison_worker_url}/js/injection.js`;
     document.body.appendChild(script);
 });
