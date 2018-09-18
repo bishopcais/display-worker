35d0c53db40719af9503c49772bd7b6c2f66ce2c
Matthew Peveler <matt.peveler@gmail.com>
2018-09-17 21:08:16 -0400

    Add uuid function for injection processes to use

--- template/injection.js
+++ template/injection.js
@@ -4,13 +4,14 @@ ipcRenderer.on('start_injection', (event, message) => {
     for (let key of Object.keys(message)) {
         window[key] = message[key];
     }
+    window[window.webview_id].uuid = require('uuid/v1');
 
     // injection script within liaison-worker handles
     // injecting any additional JS scripts and setting up
     // the listeners, etc. so that we never have to touch this
     // script again.
     let script = document.createElement('script');
-    script.src = `${window.liaison_worker_url}/js/injection.js`;
+    script.src = `${window.liaison_worker_url}/js/injection.js?time=${Date.now()}`;
     script.onerror = (error) => {
         console.error(`The requested script ${error.target.src} failed to load...`);
     };
